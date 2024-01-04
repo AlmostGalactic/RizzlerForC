4c2611db2a39eb4ec6d63f3686e07e937e497b40
AlmostGalactic <143559054+AlmostGalactic@users.noreply.github.com>
2024-01-04 17:03:41 -0600

    Create SimpleNamePrinter.c

---
+++ SimpleNamePrinter.c
@@ -0,0 +1,13 @@
+#include "rizzler.h"
+
+rizz chief W L lol
+
+  grades name WRizz 25 LRizz rn
+
+  yap W "Whats your name?: \n" L rn
+  detect W name skibidi 25 skibidi stdin L rn
+
+  yap W "Hello, %s" skibidi name L rn
+
+  give 0 rn
+cuh
